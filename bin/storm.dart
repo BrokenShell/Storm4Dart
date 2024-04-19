e3060646a45339fddf5c3a198f38e994376b01c1
BrokenShell <webmaster@sharpdesigndigital.com>
2024-04-19 14:27:11 -0700

    Initial commit

---
+++ bin/storm.dart
@@ -0,0 +1,67 @@
+import 'package:storm/storm.dart' as storm;
+
+void main(List<String> arguments) {
+  // Utilities
+  print("Storm Version: ${storm.version()}");
+  print("Min Int: ${storm.minInt()}");
+  print("Max Int: ${storm.maxInt()}");
+  print("Min Float: ${storm.minFloat()}");
+  print("Max Float: ${storm.maxFloat()}");
+  print("Min Below: ${storm.minBelow()}");
+  print("Min Above: ${storm.minAbove()}");
+
+  // Random Floats
+  print("Canonical: ${storm.canonical()}");
+  print("Uniform (0, 1): ${storm.uniform(0.0, 1.0)}");
+  print("Exponential (Rate=1.0): ${storm.exponential(1.0)}");
+  print("Gamma (Shape=2.0, Scale=2.0): ${storm.gamma(2.0, 2.0)}");
+  print("Weibull (Shape=1.0, Scale=2.0): ${storm.weibull(1.0, 2.0)}");
+  print("Normal (Mean=0.0, Std Dev=1.0): ${storm.normal(0.0, 1.0)}");
+  print("Log-Normal (Mean=0.0, Std Dev=1.0): ${storm.logNormal(0.0, 1.0)}");
+  print("Extreme Value (Location=0.0, Scale=1.0): ${storm.extremeValue(0.0, 1.0)}");
+  print("Chi-Squared (Degrees of Freedom=2): ${storm.chiSquared(2)}");
+  print("Cauchy (Location=0.0, Scale=1.0): ${storm.cauchy(0.0, 1.0)}");
+  print("Fisher F (DF1=2, DF2=2): ${storm.fisherF(2, 2)}");
+  print("Student T (Degrees of Freedom=5): ${storm.studentT(5)}");
+  print("Beta (Alpha=0.5, Beta=0.5): ${storm.beta(0.5, 0.5)}");
+  print("Pareto (Alpha=2.0): ${storm.pareto(2.0)}");
+  print("Von Mises (Mu=0.0, Kappa=1.0): ${storm.vonmises(0.0, 1.0)}");
+  print("Triangular (Low=0.0, High=1.0, Mode=0.5): ${storm.triangular(0.0, 1.0, 0.5)}");
+
+  // Random Boolean
+  print("Bernoulli (P=0.75): ${storm.bernoulli(0.75)}");
+  print("Percent True (25.0): ${storm.percentTrue(25.0)}");
+
+  // Random Integer
+  print("Uniform Int Variate (-50, 50): ${storm.uniformInt(-50, 50)}");
+  print("Binomial Variate (10 trials, p=0.5): ${storm.binomial(10, 0.5)}");
+  print("Negative Binomial Variate (5 trials, p=0.3): ${storm.negativeBinomial(5, 0.3)}");
+  print("Geometric Variate (p=0.5): ${storm.geometric(0.5)}");
+  print("Poisson Variate (mean=4): ${storm.poisson(4)}");
+  print("Random Below (100): ${storm.randomBelow(100)}");
+  print("Random Range (1, 100, 5): ${storm.randomRange(1, 100, 5)}");
+  print("Plus or Minus (10): ${storm.plusOrMinus(10)}");
+  print("Plus or Minus Linear (20): ${storm.plusOrMinusLinear(20)}");
+  print("Plus or Minus Gauss (30): ${storm.plusOrMinusGauss(30)}");
+
+  // Random Dice
+  print("Roll a D20: ${storm.d(20)}");
+  print("Roll three six-sided dice: ${storm.dice(3, 6)}");
+  print("Roll 4d6, take top three: ${storm.abilityDice(4)}");
+
+  // Random Index
+  print("Random Index (10): ${storm.randomIndex(10)}");
+  print("Front Linear (10): ${storm.frontLinear(10)}");
+  print("Back Linear (10): ${storm.backLinear(10)}");
+  print("Middle Linear (10): ${storm.middleLinear(10)}");
+  print("Quantum Linear (10): ${storm.quantumLinear(10)}");
+  print("Front Gauss (10): ${storm.frontGauss(10)}");
+  print("Middle Gauss (10): ${storm.middleGauss(10)}");
+  print("Back Gauss (10): ${storm.backGauss(10)}");
+  print("Quantum Gauss (10): ${storm.quantumGauss(10)}");
+  print("Front Poisson (10): ${storm.frontPoisson(10)}");
+  print("Back Poisson (10): ${storm.backPoisson(10)}");
+  print("Middle Poisson (10): ${storm.middlePoisson(10)}");
+  print("Quantum Poisson (10): ${storm.quantumPoisson(10)}");
+  print("Quantum Monty (10): ${storm.quantumMonty(10)}");
+}
---
+++ lib/storm.dart
@@ -0,0 +1,223 @@
+import 'dart:ffi' as ffi;
+import 'package:ffi/ffi.dart' as ffi;
+import 'dart:io';
+
+final storm = ffi.DynamicLibrary.open("${Directory.current.path}/lib/storm.so");
+
+// Storm Version
+String version() {
+  final getVersion = storm.lookupFunction<
+      ffi.Pointer<ffi.Utf8> Function(),
+      ffi.Pointer<ffi.Utf8> Function()>("get_version");
+  return getVersion().cast<ffi.Utf8>().toDartString();
+}
+
+// Utility Functions
+final minInt = storm.lookupFunction<
+    ffi.Int64 Function(),
+    int Function()>("min_int");
+
+final maxInt = storm.lookupFunction<
+    ffi.Int64 Function(),
+    int Function()>("max_int");
+
+final minFloat = storm.lookupFunction<
+    ffi.Double Function(),
+    double Function()>("min_float");
+
+final maxFloat = storm.lookupFunction<
+    ffi.Double Function(),
+    double Function()>("max_float");
+
+final minBelow = storm.lookupFunction<
+    ffi.Double Function(),
+    double Function()>("min_below");
+
+final minAbove = storm.lookupFunction<
+    ffi.Double Function(),
+    double Function()>("min_above");
+
+// Random Floats
+final canonical = storm.lookupFunction<
+    ffi.Double Function(),
+    double Function()>("canonical_variate");
+
+final uniform = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("uniform_real_variate");
+
+final exponential = storm.lookupFunction<
+    ffi.Double Function(ffi.Double),
+    double Function(double)>("exponential_variate");
+
+final gamma = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("gamma_variate");
+
+final weibull = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("weibull_variate");
+
+final normal = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("normal_variate");
+
+final logNormal = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("log_normal_variate");
+
+final extremeValue = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("extreme_value_variate");
+
+final chiSquared = storm.lookupFunction<
+    ffi.Double Function(ffi.Double),
+    double Function(double)>("chi_squared_variate");
+
+final cauchy = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("cauchy_variate");
+
+final fisherF = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("fisher_f_variate");
+
+final studentT = storm.lookupFunction<
+    ffi.Double Function(ffi.Double),
+    double Function(double)>("student_t_variate");
+
+final beta = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("beta_variate");
+
+final pareto = storm.lookupFunction<
+    ffi.Double Function(ffi.Double),
+    double Function(double)>("pareto_variate");
+
+final vonmises = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double),
+    double Function(double, double)>("vonmises_variate");
+
+final triangular = storm.lookupFunction<
+    ffi.Double Function(ffi.Double, ffi.Double, ffi.Double),
+    double Function(double, double, double)>("triangular_variate");
+
+// Random Boolean
+final bernoulli = storm.lookupFunction<
+    ffi.Bool Function(ffi.Double),
+    bool Function(double)>("bernoulli_variate");
+
+final percentTrue = storm.lookupFunction<
+    ffi.Bool Function(ffi.Double),
+    bool Function(double)>("percent_true");
+
+// Random Integers
+final uniformInt = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64, ffi.Int64),
+    int Function(int, int)>("uniform_int_variate");
+
+final binomial = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64, ffi.Double),
+    int Function(int, double)>("binomial_variate");
+
+final negativeBinomial = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64, ffi.Double),
+    int Function(int, double)>("negative_binomial_variate");
+
+final geometric = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Double),
+    int Function(double)>("geometric_variate");
+
+final poisson = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Double),
+    int Function(double)>("poisson_variate");
+
+final randomBelow = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("random_below");
+
+final randomRange = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64, ffi.Int64, ffi.Int64),
+    int Function(int, int, int)>("random_range");
+
+final plusOrMinus = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("plus_or_minus");
+
+final plusOrMinusLinear = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("plus_or_minus_linear");
+
+final plusOrMinusGauss = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("plus_or_minus_gauss");
+
+// Random Dice
+final d = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("d");
+
+final dice = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64, ffi.Int64),
+    int Function(int, int)>("dice");
+
+final abilityDice = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("ability_dice");
+
+// Random Index
+final randomIndex = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("random_index");
+
+final frontLinear = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("front_linear");
+
+final backLinear = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("back_linear");
+
+final middleLinear = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("middle_linear");
+
+final quantumLinear = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("quantum_linear");
+
+final frontGauss = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("front_gauss");
+
+final middleGauss = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("middle_gauss");
+
+final backGauss = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("back_gauss");
+
+final quantumGauss = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("quantum_gauss");
+
+final frontPoisson = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("front_poisson");
+
+final backPoisson = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("back_poisson");
+
+final middlePoisson = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("middle_poisson");
+
+final quantumPoisson = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("quantum_poisson");
+
+final quantumMonty = storm.lookupFunction<
+    ffi.Int64 Function(ffi.Int64),
+    int Function(int)>("quantum_monty");
